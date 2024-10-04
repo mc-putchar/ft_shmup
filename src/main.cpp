01c1f0c225b67fa22a39d5d94d9002113833fb3c
Martin Cutura <mcutura@student.42berlin.de>
2024-10-04 20:56:23 +0200

    add: ncurses init

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,16 @@
 int main(int ac, char **av) {
   if (ac > 1)
     std::cout << "Usage: " << av[0] << std::endl;
+  (void) initscr();
+  keypad(stdscr, TRUE);
+  (void) nonl();
+  (void) cbreak();
+  //(void) noecho();
+  echo();
+  while (1) {
+    int c = getch();
+    printw("Pressed: %d\n", c);
+  }
+  endwin();
   return (0);
 }
